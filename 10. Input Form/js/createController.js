09f0f186310f96ac51567cb2e0e2197f66491d41
Miloš Slađan Davitković <davitkooo@gmail.com>
2015-12-18 14:14:21 +0100

    All datePicker are working now

--- 10. Input Form/js/createController.js
+++ 10. Input Form/js/createController.js
@@ -4,9 +4,7 @@ app.controller('CreateController', ['$scope', function ($scope) {
 
 
 
-
-
-
+    //Reset function
     $scope.blankConfig = {};
 
     $scope.reset = function () {
@@ -14,18 +12,60 @@ app.controller('CreateController', ['$scope', function ($scope) {
     };
 
     $scope.reset();
+    //--------------------------
 
-
-
-
-
-    $scope.today = function () {
-        $scope.dt = new Date();
+    // Date picker 
+    $scope.today = function (whichDate) {
+        switch (whichDate) {
+        case 'create':
+            $scope.dt.create = new Date();
+            break;
+        case 'start':
+            $scope.dt.start = new Date();
+            break;
+        case 'expiration':
+            $scope.dt.expiration = new Date();
+            break;
+        default:
+            $scope.dt = new Date();
+        }
     };
+
+    $scope.today = function (whichDate) {
+
+        switch (whichDate) {
+        case 'create':
+            $scope.configuration.createDate = new Date();
+            break;
+        case 'start':
+            $scope.configuration.startDate = new Date();
+            break;
+        case 'expiration':
+            $scope.configuration.expirationDate = new Date();
+            break;
+        default:
+            $scope.configuration.Date = new Date();
+        }
+    };
+
     $scope.today();
 
-    $scope.clear = function () {
-        $scope.dt = null;
+    $scope.clear = function (whichDate) {
+        switch (whichDate) {
+        case 'create':
+            $scope.configuration.createDate = '';
+            break;
+        case 'start':
+            $scope.configuration.startDate = '';
+            break;
+        case 'expiration':
+            $scope.configuration.expirationDate = '';
+            break;
+        default:
+            $scope.dt = null;
+        }
+
+
     };
 
     // Disable weekend selection
@@ -39,12 +79,27 @@ app.controller('CreateController', ['$scope', function ($scope) {
     $scope.toggleMin();
     $scope.maxDate = new Date(2020, 5, 22);
 
-    $scope.open = function ($event) {
-        $scope.status.opened = true;
+    $scope.open = function ($event, id) {
+
+        switch (id) {
+        case 'create':
+            $scope.status.create.opened = true;
+            break;
+        case 'start':
+            $scope.status.start.opened = true;
+            break;
+        case 'expiration':
+            $scope.status.expiration.opened = true;
+            break;
+        default:
+            $scope.status.opened = true;
+        }
+
     };
 
     $scope.setDate = function (year, month, day) {
-        $scope.dt = new Date(year, month, day);
+        //        $scope.dt = new Date(year, month, day);
+        $scope.configuration.createDate = new Date(year, month, day);
     };
 
     $scope.dateOptions = {
@@ -56,6 +111,18 @@ app.controller('CreateController', ['$scope', function ($scope) {
     $scope.format = $scope.formats[0];
 
     $scope.status = {
+        opened: false
+    };
+
+    $scope.status.create = {
+        opened: false
+    };
+
+    $scope.status.start = {
+        opened: false
+    };
+
+    $scope.status.expiration = {
         opened: false
     };
 
